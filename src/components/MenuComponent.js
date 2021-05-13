286de988b49e7d2335d75dca2f9c26826b6193bc
Ismael Ávila (Isma) <avila2285@hotmail.com>
2021-05-13 11:29:16 -0500

    Functional components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,61 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import DishDetail from './dishDetailComponent';
 
-class Menu extends Component {
+function RenderMenuItem({dish, selectedDishFunction}){
+  return(
+    <Card key={dish.id} onClick={()=>selectedDishFunction(dish.id)}>
+      <CardImg width="100%" src={dish.image} alt={dish.name} />
+      <CardImgOverlay>
+        <CardTitle>{dish.name}</CardTitle>
+      </CardImgOverlay>
+    </Card>
 
-    /*Here is where you receive the data that is passed to App.js*/
+  );
+}
 
-    //Part 1: Contructor and the data you receive
-    constructor(props) {
-      super(props);
-      console.log("Menu Component constructor invoked")
+/*
+  In this function you're receiving data from MainComponent.js when this function is
+  called.
+*/
+const Menu = (props) =>{
+  //Main function to render
+  //The third part of a component: Return a part of the DOM
 
-    }
-
-    componentDidMount(){
-      console.log("Menu Component componentDidMount invoked")
-    }
-
-
-    /*Functions to change parameters or states*/
-
-      
-    //Function to render 1
-    renderDish(dish){
-      return(
-        <DishDetail dish ={dish}/>
-      );
-    }
-
-
-    //Main function to render
-    //The third part of a component: Return a part of the DOM
-    render() {
-      console.log("Menu Component render invoked");
-      const menu = this.props.dishes.map((dish) => {
-        return (
-          <div  className="col-12 col-md-5 m-1">
-            <Card key={dish.id} onClick={()=>this.props.selected(dish.id)}>
-              <CardImg width="100%" src={dish.image} alt={dish.name} />
-              <CardImgOverlay>
-                <CardTitle>{dish.name}</CardTitle>
-              </CardImgOverlay>
-            </Card>
-          </div>
-        );
-      });
+  console.log("Menu Component render invoked");
+  //<RenderMenuItem dish={dish} selectedDish={props.selected}/>
+  //<RenderMenuItem tranfered_data passed_function_from MainComponent/>
+  const menu = props.dishes.map((dish) => {
+    return (
+      <div  className="col-12 col-md-5 m-1">
+        <RenderMenuItem dish={dish} selectedDishFunction={props.selected}/>
+      </div>
+    );
+  });
 
 
 
-      return (
-        <div className="container">
-          <div className="row">
-            {menu}
-          </div>
-        </div>
-      );
-    }
+  return (
+    <div className="container">
+      <div className="row">
+        {menu}
+      </div>
+    </div>
+  );
+
+
 }
 
 export default Menu;
--- src/components/dishDetailComponent.js
+++ src/components/dishDetailComponent.js
@@ -1,44 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component{
 
-	constructor(props){
-		super(props);
-	
-		this.state = {
-			
-		};
+function RenderDish({dish}){
+	return(
+		<div className="col-12 col-md-5 m-1">
+			<Card>
+        <CardImg top src={dish.image} alt={dish.name} />  
+        
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+		</div>
+		
+	);
 
-	}
 
-	componentDidMount(){
-    console.log("DishDetail Component componentDidMount invoked");
-  }
+}
 
-  componentDidUpdate(){
-  	console.log("DishDetail Component componentDidUpdate invoked");
-  }
-
-	renderDish(dish){
-		return(
-			<div className="col-12 col-md-5 m-1">
-				<Card>
-	        <CardImg top src={dish.image} alt={dish.name} />  
-	        
-	        <CardBody>
-	          <CardTitle>{dish.name}</CardTitle>
-	          <CardText>{dish.description}</CardText>
-	        </CardBody>
-	      </Card>
-			</div>
-			
-		);
-
-	}
-
-	renderComments(dish){
-		if(dish.comments.length !== 0){
+function RenderComments({dish}){
+	if(dish.comments.length !== 0){
 			return(
 				<div className="col-12 col-md-5 m-1">
 					<h4>Comments</h4>
@@ -65,20 +48,18 @@ class DishDetail extends Component{
 				</div>
 			);
 		}
-	}
 
-	//Functions to work
+}
 
-	render(){
-
-		console.log("DishDetail Component render invoked");
+const DishDetail = (props)=>{
+	console.log("DishDetail Component render invoked");
 		
-		if(this.props.dish != null){
+		if(props.dish != null){
 			return(
 			<div class = "container">
 				<div className="row">
-					{this.renderDish(this.props.dish)}
-					{this.renderComments(this.props.dish)}
+					<RenderDish dish ={props.dish}/>
+					<RenderComments dish ={props.dish}/>
 				</div>
 			</div>
 
@@ -90,7 +71,6 @@ class DishDetail extends Component{
 	          <div></div>
 	        );
 		}
-	}
-}
 
+}
 export default DishDetail;
