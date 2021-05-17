012a8c5039c173d5a59832ff7d31215ee479fa1f
Ismael Ávila (Isma) <avila2285@hotmail.com>
2021-05-17 17:40:01 -0500

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';   //To nav between diferent views
 import {DISHES} from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import Header from "./HeaderComponent"
 import Footer from "./FooterComponent"
 import Home from "./HomeComponent"
+import Contact from "./ContactComponent"
 
 
 /*So that, the main component is now responsible fore verything related to
@@ -16,18 +20,41 @@ class Main extends Component{
     super(props);
 
     this.state = {
-      dishes:DISHES,
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   } 
 
   //<Menu dishes={this.state.dishes} selected={(dishId)=>this.onDishSelect(dishId)}/>
+
+  /*
+  Note:
+
+  If you're not going to pass parameters to the components, then you can use the next
+  sintaxis:
+
+  <Route path='/home' component={()=><Home/>} /> or
+  <Route exact path='/contactus' component={Contact}/>
+
+  But if you need to pass parameters to the function, then you need to use this sintaxis:
+  <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+
+
+  */
   render(){
     return(
       <div className="App">
         <Header/>
         <Switch>
-          <Route path='/home' component={()=><Home/>} />
+          <Route path='/home' component={() => <Home 
+            dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+            promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+            leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path='/contactus' component={Contact}/>
           <Redirect to="/home" />
         </Switch>
         <Footer/>
