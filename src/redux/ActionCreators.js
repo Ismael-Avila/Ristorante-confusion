0e85681480baa5ce27f17e43ea1b1404bf0e7124
Ismael Ávila (Isma) <avila2285@hotmail.com>
2021-06-09 13:42:48 -0500

    Fetch Handling Erros

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -33,8 +33,46 @@ export const fetchDishes = () => (dispatch) => {
   */
 
   return fetch(baseUrl + 'dishes')
+    /*
+    So, when you return the response, what happens? This response then will
+    become available to the next "Then". So, if you know how promises work,
+    so when this returns something, that will be delivered in as the incoming
+    parameter to the next "Then" that has been changed into the set of promise
+    handlers here.
+    */
+    .then(response => {
+      if (response.ok){
+        return response;
+      }
+      else {
+        /*So, the first part is a situation where you receive a response from
+        the server, but the response could be an error response from the server.*/
+        var error = new Error('Error ' + response.status + ': ' + response.statusText);
+        error.response = response;
+        throw error;
+      }},
+      /*
+      The second part would be a situation where you don't hear back anything
+      from the server, so that's where you will have to handle the error
+      appropriate.
+      */
+      error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+      })
     .then(response => response.json())      //Requeste the response
-    .then(dishes => dispatch(addDishes(dishes))); //Take the response and convert it as dishes parameter.
+    .then(dishes => dispatch(addDishes(dishes))) //Take the response and convert it as dishes parameter.
+    /*
+    So, when you throw the error in a promise handler, we can then implement
+    a catch at the bottom which will catch the error and then handle the error
+    appropriately.
+
+    Now, in the handling of promises, an error will cause a rejected promise.
+    So, that we will catch using the.catch of the promises. So, inside the
+    .catch you will receive the error. And this catch would be caused either
+    because you throw an error in the first or second part.
+    */
+    .catch(error => dispatch(dishesFailed(error.message)));
 }
 
 //Inform somebody, saying that the dishes are beggining to be loaded and
@@ -59,8 +97,23 @@ export const addDishes = (dishes) => ({
 
 export const fetchComments = () => (dispatch) => {    
   return fetch(baseUrl + 'comments')
+    .then(response => {
+      if (response.ok){
+        return response;
+      }
+      else {
+        var error = new Error('Error ' + response.status + ': ' + response.statusText);
+        error.response = response;
+        throw error;
+      }},
+      error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+      }
+    )
     .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)));
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)));
 };
 
 export const commentsFailed = (errmess) => ({
@@ -82,8 +135,23 @@ export const fetchPromos = () => (dispatch) => {
   dispatch(promosLoading());
 
   return fetch(baseUrl + 'promotions')
+    .then(response => {
+      if (response.ok){
+        return response;
+      }
+      else {
+        var error = new Error('Error ' + response.status + ': ' + response.statusText);
+        error.response = response;
+        throw error;
+      }},
+      error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+      }
+    )
     .then(response => response.json())
-    .then(promos => dispatch(addPromos(promos)));
+    .then(promos => dispatch(addPromos(promos)))
+    .catch(error => dispatch(promosFailed(error.message)));
 }
 
 export const promosLoading = () => ({
